da43f193661eeb4ce6828eb0a0e29a9483c6808a
João Lopes <83359884+Jolmicek@users.noreply.github.com>
2026-07-21 01:28:08 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,16 +1,16 @@
-// O segredo está aqui: declarar na raiz do ficheiro para o shared_preferences ver!
-rootProject.extra["kotlin_version"] = "1.9.22"
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+
+    // OPÇÃO NUCLEAR: Obriga TODOS os plugins (incluindo o shared_preferences)
+    // a usar o Kotlin 1.9.22, quer eles queiram ou não.
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlin") {
+                useVersion("1.9.22")
+            }
+        }
     }
 }
