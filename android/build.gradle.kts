644a8f2b9bead562ea31d5b3286a3e110230baa0
João Lopes <83359884+Jolmicek@users.noreply.github.com>
2026-07-21 01:35:44 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,13 +4,16 @@ allprojects {
         mavenCentral()
     }
 
-    // OPÇÃO NUCLEAR: Obriga TODOS os plugins (incluindo o shared_preferences)
-    // a usar o Kotlin 1.9.22, quer eles queiram ou não.
     configurations.all {
+        // A nossa regra anterior para forçar a versão
         resolutionStrategy.eachDependency {
             if (requested.group == "org.jetbrains.kotlin") {
                 useVersion("1.9.22")
             }
         }
+        
+        // NOVO: Remove os pacotes antigos que estão a causar os duplicados
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
     }
 }
