962c0063e5719c5ed210069b2bb7f9c4ec9f0b64
João Lopes <83359884+Jolmicek@users.noreply.github.com>
2026-07-21 02:32:38 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,15 +5,20 @@ allprojects {
     }
 
     configurations.all {
-        // A nossa regra anterior para forçar a versão
-        resolutionStrategy.eachDependency {
-            if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("1.9.22")
+        resolutionStrategy {
+            // Força todas as dependências do Kotlin a usarem a versão moderna unificada
+            force("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+            force("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.22")
+            
+            // Rejeita e descarta completamente as versões antigas dos módulos JDK separatistas
+            exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+            exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+            
+            eachDependency {
+                if (requested.group == "org.jetbrains.kotlin") {
+                    useVersion("1.9.22")
+                }
             }
         }
-        
-        // NOVO: Remove os pacotes antigos que estão a causar os duplicados
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
     }
 }
