61cff6fff3b3a6ffc4be2bba8df90f2a7269d08b
João Lopes <83359884+Jolmicek@users.noreply.github.com>
2026-07-21 01:16:30 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    // Definimos a variável para que os plugins antigos do Flutter a consigam ler
+    extra["kotlin_version"] = "1.9.22" 
+    
+    repositories {
+        google()
+        mavenCentral()
+    }
+    // NOTA IMPORTANTE: O bloco 'dependencies' com o classpath NÃO deve estar aqui!
+}
+
 allprojects {
     repositories {
         google()
