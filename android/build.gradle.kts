a8cc9f4b779172053daa66036bdcfd00ba1df3a7
João Lopes <83359884+Jolmicek@users.noreply.github.com>
2026-07-21 01:11:35 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,3 @@
-buildscript {
-    extra["kotlin_version"] = "1.9.22"
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
-        classpath("com.android.tools.build:gradle:8.1.0")
-    }
-}
-
 allprojects {
     repositories {
         google()
